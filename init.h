8ddd367755279de08cae83f2cd2b2bc18b5e281b
EwenQuim <ewen.quimerch@gmail.com>
2021-11-02 15:14:13 +0100

    Revert "formatting"
    This reverts commit c8a7c980bea6589bab21477600923b7712f7a52c.

--- init.h
+++ init.h
@@ -6,15 +6,16 @@
 #ifndef __MATPROD_INIT__
 #define __MATPROD_INIT__
 
-void LocalMatrixInit(void);  // Data init
 
-void usage(int ExitCode, FILE *std);  // Cmd line parsing and usage
+void LocalMatrixInit(void);                      // Data init
+
+void usage(int ExitCode, FILE *std);             // Cmd line parsing and usage
 void CommandLineParsing(int argc, char *argv[]);
 
-void PrintResultsAndPerf(double dk, double dt, double dkt,  // Res printing
-                         double gfk, double gfkt, double bwt, int ongpu);
+void PrintResultsAndPerf(double dk, double dt, double dkt,    // Res printing
+                         double gfk, double gfkt, double bwt, int ongpu); 
 
-void CheckResults(void);  // Res checking
+void CheckResults(void); // Res checking
 
 #endif
 
